52a48f9632b4e33ef1a61d7375cd73ca879a3f02
saurabhsharma3089 <saurabhsharma3089@gmail.com>
2025-06-26 09:19:00 +0530

    new

--- script.js
+++ script.js
@@ -1,11 +1,2 @@
-// const navbarLinks = document.querySelectorAll(".navbar .nav-link");
-const menuOpenButton = document.querySelector(".menu-open-button");
-const menuCloseButton = document.querySelector(".menu-close-button");
+// // const navbarLinks = document.querySelectorAll(".navbar .nav-link");
 
-menuOpenButton.addEventListener('click', () => {
-    document.body.classList.add("show-mobile-menu");
-});
-
-menuCloseButton.addEventListener('click', () => {
-    document.body.classList.remove("show-mobile-menu");
-});
